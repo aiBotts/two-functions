36d3219bcef18e8a184115258c636316dfc705cc
J <107528288+aiBotts@users.noreply.github.com>
2024-03-06 22:15:13 -0600

    Add files via upload

---
+++ create_two_functions.py
@@ -0,0 +1,17 @@
+# This program has two functions
+# First we define the main function.
+
+def main():
+    print('I have something for you')
+    message()
+    print('Goodbye!')
+
+# Next we define the message function.
+
+def message():
+    print('I am J, ')
+    print('King of Rocket league!!')
+
+# Call the main function.
+
+main()
